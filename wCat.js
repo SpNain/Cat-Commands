cf0e87155e58d4bb8076c3505b524fe57411d966
HardeepNain <hnspnain@gmail.com>
2024-02-21 17:38:27 +0530

    removed all the comments

--- wCat.js
+++ wCat.js
@@ -1,51 +1,32 @@
-// let clicmds = process; # https://bit.ly/3WJA6km
-// console.log(clicmds);
-  
-const { getFilesData, applySFlag, applyBFlag, applyNFlag } = require("./util"); // jis jis  ka naam likh rkha h wo wo m.e k obj se uthkar yaha aa jaayega
-let contents = process.argv.slice(2);  //contents me process array se flag aur files ko as a array store karwa liya humne
+const { getFilesData, applySFlag, applyBFlag, applyNFlag } = require("./util");
+let contents = process.argv.slice(2);
 
-const flags = [];  //flag initialisation
-const files = [];   //files initialisation
+const flags = [];
+const files = [];
 
-for (let i = 0; i < contents.length; i++) {  // ye loop flag aur files ko distinguish kar dega
-    // "-s" , "-b" , "f1.txt" , "f2.txt" , "-n"
-  if (contents[i].startsWith("-")) {  // jiske base pe distinguidh krenge
+for (let i = 0; i < contents.length; i++) {
+  if (contents[i].startsWith("-")) {
     flags.push(contents[i]);
   } else {
     files.push(contents[i]);
   }
 }
 
-// -s -b -n
-// f1.txt // f2.txt
+let filesData = getFilesData(files);
 
-let filesData = getFilesData(files);  //yaha se fxn call kiya , ab jo fxn util me likh rkha h  uske hisab se kaam dega ye
-
-if (flags.includes("-s")) {  // check krega ki kahi lag aaray me -s flag h ki nhi
-  // filesData updated if s flag is present !
-  filesData = applySFlag(filesData);  // jo getFilesData se aaya hoga uspe -s flag lag jaayega
+if (flags.includes("-s")) {
+  filesData = applySFlag(filesData);
 }
-// ab filesData me -s flag lag chuka h aur filesData update ho gya h
-// console.log(filesData);
 
-// when both -b and -n flags are present
-if (flags.includes("-b") && flags.includes("-n")) {  // check ki kahi dono flag  to nhi  
-  if (flags.indexOf("-b") < flags.indexOf("-n")) {  // agr h to index k base pe priorty milegi
-    // apply b flag  
-    filesData = applyBFlag(filesData);  
+if (flags.includes("-b") && flags.includes("-n")) {
+  if (flags.indexOf("-b") < flags.indexOf("-n")) {
+    filesData = applyBFlag(filesData);
   } else {
-    // apply n flag
     filesData = applyNFlag(filesData);
   }
-}
-// only -b flag is present
-else if (flags.includes("-b")) {
-  // apply b flag
+} else if (flags.includes("-b")) {
   filesData = applyBFlag(filesData);
-}
-// only -n flag is present
-else if (flags.includes("-n")) {
-  // apply n flag
+} else if (flags.includes("-n")) {
   filesData = applyNFlag(filesData);
 }
 
